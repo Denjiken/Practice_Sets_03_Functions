b53ebc20e9705186a3d61a6aa2697a0cb2e040da
Denjiken <armanvicente4@gmail.com>
2024-10-06 15:06:10 +0800

    done

---
+++ AreaCalculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AreaCalculator
+{
+    class Program
+    {
+        public static double CalculateArea(double a, double b, double c)
+        {
+            double sm = (a + b + c) / 2;
+            double area = Math.Sqrt(sm * (sm - a) * (sm - b) * (sm - c));
+            return area;
+        }
+        static void Main(string[] args)
+        {
+            double a = 4;
+            double b = 6;
+            double c = 7;
+
+            double area = CalculateArea(a, b, c);
+            Console.WriteLine("The area of a triangle is: " + area);
+        }
+    }
+}
---
+++ LeapYear/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            bool leapYear = LeapYear(year);
+            if (leapYear)
+            {
+                Console.WriteLine($"{year} is leapyear.");
+            }
+            else
+            {
+                Console.WriteLine($"{year} is not a leapyear.");
+            }
+        }
+        static bool LeapYear(int year)
+        {
+            if (year % 4 == 0)
+            {
+                if (year % 100 == 0 && year % 400 != 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
